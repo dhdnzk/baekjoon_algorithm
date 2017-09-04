d51f97ab84faa84cc11b4d4c156c325758475b59
Donghoon Im <dongttang@gmail.com>
2017-09-04 15:03:14 +0900

    2017_09_04

--- 11655.py
+++ 11655.py
@@ -1,1 +1,22 @@
-print("a")
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+raw_data = input("")
+encrypted_data = ""
+
+for i in range(0, len(raw_data)):
+    capital_flag = False
+    if raw_data[i].isalpha():
+
+        if raw_data[i].isupper():
+            capital_flag = True
+
+        idx = (alphabet.index(raw_data[i].lower()) + 13) % 26
+
+        if capital_flag:
+            encrypted_data += alphabet[idx].upper()
+        else:
+            encrypted_data += alphabet[idx]
+
+    else:
+        encrypted_data += raw_data[i]
+
+print(encrypted_data)
